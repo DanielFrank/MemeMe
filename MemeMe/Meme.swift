4a329f989ac2d7f2fc5de6af30714a482a66f375
Daniel Frank <daniel.frank@lin-digital.com>
2015-05-05 22:09:56 -0700

    Add Meme object and add meme collection to AppDelegate

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -6,4 +6,20 @@
 //  Copyright (c) 2015 Daniel Frank. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meme {
+    
+    var topString: String?
+    var bottomString: String?
+    var originalImage: UIImage?
+    var memedImage: UIImage?
+    
+    init(topString :String, bottomString:String, image: UIImage, memedImage: UIImage) {
+        self.topString = topString
+        self.bottomString = bottomString
+        self.originalImage = image
+        self.memedImage = image
+    }
+    
+}
